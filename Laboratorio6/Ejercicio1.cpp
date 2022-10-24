8019dfdd4aa0994fdd85bac7cd40940fad3515d0
iamvegagg <00052220@uca.edu.sv>
2022-10-24 09:49:26 -0600

    Agregando ejercicio 1 ya resuelto

--- Laboratorio6/Ejercicio1.cpp
+++ Laboratorio6/Ejercicio1.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main( )
+int main()
 {
-     
+    char texto[200], subtexto[200];
+    char *busqueda;
+    cout << "Ingrese un texto: ";
+    cin >> texto;
+    cout << "ingrese la palabra a buscar: ";
+    cin >> subtexto;
+    busqueda = strstr(texto, subtexto);
+    if (busqueda == NULL)
+    {
+        cout << "La palabra no existe dentro del texto.";
+    }
+    else
+    {
+        cout << "La palabra se encuentra dentro del texto.";
+    }
+
     return 0;
 }
